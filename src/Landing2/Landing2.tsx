8cf6e9c757ec1e4fc2ad938798f0e7dbca7a234a
abao929 <44300045+abao929@users.noreply.github.com>
2022-06-21 20:55:44 -0400

    landing 2 done

--- src/Landing2/Landing2.tsx
+++ src/Landing2/Landing2.tsx
@@ -2,5 +2,20 @@ import React from 'react';
 import './Landing2.css';
 
 export default function Landing2() {
-  return <div>Landing2</div>;
+  return (
+    <div className="landing-container">
+      <div className="lhs">
+        <div className="row">div.</div>
+      </div>
+      <div className="rhs">
+        <div className="box">
+          <div className="shape">
+            <div className="semi-circle">
+              {/* One of my favorite works from the MOMA */}
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
 }
