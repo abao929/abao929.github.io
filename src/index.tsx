1ec0e7de1531d53838e648689b333d5174bf2a07
abao929 <44300045+abao929@users.noreply.github.com>
2022-06-30 20:15:15 -0400

    react-router

--- src/index.tsx
+++ src/index.tsx
@@ -5,12 +5,18 @@ import App from './App';
 import Landing from './Landing/Landing';
 import Gallery from './Gallery/Gallery';
 import Landing2 from './Landing2/Landing2';
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
 
 const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement
 );
 root.render(
   <React.StrictMode>
-    <Landing2 />
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Landing />} />
+        <Route path="gallery" element={<Gallery />} />
+      </Routes>
+    </BrowserRouter>
   </React.StrictMode>
 );
