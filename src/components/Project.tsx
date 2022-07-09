57ec7057b1027189f8ae2e819ef9cb1aed7289ba
abao929 <44300045+abao929@users.noreply.github.com>
2022-07-09 15:56:46 -0400

    working collapse animation

--- src/components/Project.tsx
+++ src/components/Project.tsx
@@ -1,6 +1,5 @@
-import React, { useEffect, useState, useRef } from 'react'
+import React, { useEffect, useState, useRef, Component } from 'react'
 
-import styles from '../styles/project.module.sass'
 import styled from 'styled-components'
 
 interface ProjectProp {
@@ -51,17 +50,61 @@ const MouseImage = styled.img`
   }
 `
 
-/* transform: translate(
-  ${mousePosition.x - imgOffset.x / 2}px,
-  ${mousePosition.y - imgOffset.y / 2}px
-); */
-
 const Panel = styled.div`
   border-bottom: 1px solid rgba(0, 0, 0, 0.3);
   margin: 0 40px;
 `
 
-const PanelHeader = styled.div``
+const PanelHeader = styled.div`
+  cursor: pointer;
+  padding: 40px 0;
+  display: flex;
+  justify-content: space-between;
+  font: 400 max(7vw, 4rem) scotch-display;
+  word-break: break-word;
+  text-transform: uppercase;
+  transition: opacity 0.6s cubic-bezier(0.165, 0.84, 0.44, 1);
+  &:hover {
+    opacity: 0.5;
+  }
+  p {
+    margin-top: auto;
+    color: rgba(150, 150, 150);
+    font: 500 1.5rem sans-serif;
+  }
+`
+
+const PanelContent = styled.div`
+  overflow: hidden;
+  transition: all 0.6s cubic-bezier(0.17, 0.67, 0.83, 0.67);
+  .image-container {
+    background-color: #f0f0f0;
+    display: flex;
+    justify-content: center;
+    padding: clamp(30px, 10vh, 300px) clamp(40px, 10vw, 400px);
+    img {
+      width: 100%;
+      height: auto;
+      box-shadow: rgba(100, 100, 111, 0.2) 0px 7px 29px 0px;
+    }
+  }
+  .text-container {
+    padding: 40px 0;
+    display: flex;
+    flex-direction: row;
+    & > * {
+      flex: 1;
+    }
+    .description {
+      & > *:first-child {
+        padding-bottom: 20px;
+      }
+    }
+  }
+  &.hidden {
+    display: none;
+  }
+`
 
 // https://florentbiffi.com/ - use this for inspiration - on expanded change the video/demo into a slideshow component
 export default function Project({
@@ -76,19 +119,12 @@ export default function Project({
   const [active, setActive] = useState(false)
   const mousePosition = useMousePosition()
   const [imgOffset, setImgOffset] = useState<Position>({ x: 0, y: 0 })
-  const [dropdownHeight, setDropdownHeight] = useState(0)
   const onImgLoad = ({ target: img }: any) => {
     const { offsetHeight, offsetWidth } = img
     setImgOffset({ x: offsetWidth, y: offsetHeight })
   }
-  // const elementRef = useRef(null)
-  // useEffect(() => {
-  //   console.log(elementRef.current?.['clientHeight'])
-  //   let height: number = elementRef.current?.['clientHeight'] ?? 0
-  //   setDropdownHeight(height)
-  // }, [])
+  const elementRef = useRef(null)
 
-  // console.log(mousePosition)
   return (
     <>
       <MouseImage
@@ -103,35 +139,33 @@ export default function Project({
         }}
         onLoad={onImgLoad}
       />
-      <div className={styles.container}>
-        <div
-          className={styles.title}
+      <Panel>
+        <PanelHeader
           onClick={() => toggleExpand(!expanded)}
           onMouseEnter={() => setActive(true)}
           onMouseLeave={() => setActive(false)}
         >
           {title}
           <p>{`${number < 10 && 0}${number}`}</p>
-        </div>
-        <div className={styles.dropdownContainer}>
-          <div
-            className={styles.dropdown}
-            // ref={elementRef}
-            // style={{ height: `${expanded ? dropdownHeight : 'auto'}` }}
-          >
-            <div className={styles.imgContainer}>
-              <img src={img} alt="" />
-            </div>
-            <div className={styles.textContainer}>
-              <div>{year}</div>
-              <div className={styles.description}>
-                <div>{desc}</div>
-                <a href={link}>VISIT FULL VERSION</a>
-              </div>
+        </PanelHeader>
+        <PanelContent
+          ref={elementRef}
+          style={{
+            height: expanded ? elementRef.current?.['scrollHeight'] : 0,
+          }}
+        >
+          <div className="image-container">
+            <img src={img} alt="" />
+          </div>
+          <div className="text-container">
+            <div>{year}</div>
+            <div className="description">
+              <div>{desc}</div>
+              <a href={link}>VISIT FULL VERSION</a>
             </div>
           </div>
-        </div>
-      </div>
+        </PanelContent>
+      </Panel>
     </>
   )
 }
