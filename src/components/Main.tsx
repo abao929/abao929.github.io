7d3655dd927a0040b4ffcc15baca6c602d95f2eb
abao929 <44300045+abao929@users.noreply.github.com>
2022-07-14 01:23:43 -0400

    trying out divider between components

--- src/components/Main.tsx
+++ src/components/Main.tsx
@@ -1,5 +1,4 @@
 import React from 'react'
-import Landing3 from '../Landing3/Landing3'
 import About from './About'
 import Projects from './Projects'
 import Landing from './Landing'
@@ -8,6 +7,15 @@ import styled from 'styled-components'
 const Container = styled.div`
   margin: 0 clamp(20px, 4vw, 40px);
   box-sizing: border-box;
+  /* Border underneath each element idk how I feel abt it tho */
+  /* & > *:not(:last-child):after {
+    content: '';
+    position: absolute;
+    left: calc(50% - 10vw);
+    bottom: -1px;
+    width: 20vw;
+    border-bottom: 2px solid black;
+  } */
 `
 
 export default function Main() {
