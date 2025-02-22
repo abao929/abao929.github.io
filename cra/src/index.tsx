5756d92508605333f12077a97aa569d8e88e1f59
abao929 <alexbao929@gmail.com>
2025-02-22 13:25:45 -0500

    why is this taking me so many tries am i dumb

--- cra/src/index.tsx
+++ cra/src/index.tsx
@@ -2,7 +2,6 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import './styles/index.sass'
 import Gallery from './fodder/Gallery/Gallery'
-import Wat from './fodder/Gallery/Wat'
 import Quiz from './fodder/Quiz/Quiz'
 import { BrowserRouter, Route, Routes } from 'react-router-dom'
 import Main from './components/Main'
@@ -15,7 +14,6 @@ root.render(
       <Routes>
         <Route path='/' element={<Main />} />
         <Route path='gallery' element={<Gallery />} />
-        <Route path='test' element={<Wat />} />
         <Route path='quiz' element={<Quiz />} />
         <Route path='new' element={<New />}></Route>
       </Routes>
