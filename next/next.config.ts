2925848e99e59e596813b3389ad74ec0868531a1
abao929 <alexbao929@gmail.com>
2025-02-22 12:34:42 -0500

    create next app

---
+++ next/next.config.ts
@@ -0,0 +1,11 @@
+import type { NextConfig } from 'next'
+
+const nextConfig = {
+  output: 'export', // Enables static export
+  distDir: 'out', // Output directory
+  basePath: '', // Ensures it's served at alexbao.net
+}
+
+module.exports = nextConfig
+
+export default nextConfig
