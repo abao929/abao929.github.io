4c2a4fced14a3a8cba6e788c9cac2b116c32f8f5
abao929 <alexbao929@gmail.com>
2025-02-27 21:42:08 -0500

    Add storybook and styled components

--- next/next.config.ts
+++ next/next.config.ts
@@ -4,6 +4,9 @@ const nextConfig: NextConfig = {
   output: 'export',
   distDir: 'out',
   basePath: '', // Keep empty if deploying to root domain
+  compiler: {
+    styledComponents: true,
+  },
 }
 
 export default nextConfig
